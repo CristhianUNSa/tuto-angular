21673968b94f40ec88ee7c77ea5d9ce071bfa3f2
Cristhian <valenciacristhian47@gmail.com>
2015-12-25 13:21:28 -0300

    VideoTutorial 6 de Israel Guzmán. Se creó una app muy simple con intercambio de datos entre controladores

--- js/Fabricas/fabricas.js
+++ js/Fabricas/fabricas.js
@@ -10,3 +10,35 @@ miApp
 		return {mensaje:'Saludos desde la fábrica'};
 	})
 	;
+var miApp2=angular.module("miApp2",[]);
+miApp2
+	.controller('ControladorUno', ['$scope','Fabrica', function($scope,Fabrica){
+		$scope.nuevo= function(){
+			Fabrica.msjNuevo($scope.mensajeNuevo);
+		};
+		$scope.dato=Fabrica.objeto;
+	}])
+	.controller('ControladorDos', ['$scope','Fabrica', function($scope,Fabrica){
+		$scope.nuevo= function(){
+			Fabrica.msjNuevo($scope.mensajeNuevo);
+		};
+		$scope.dato=Fabrica.objeto;
+	}])
+	.controller('ControladorTres', ['$scope','Fabrica', function($scope,Fabrica){
+		$scope.resetear=function(){
+			Fabrica.msjInicial();	
+		}
+	}])
+	.factory('Fabrica', function(){
+		var servicio={
+			objeto:{mensaje:'Saludos desde la fábrica !'},
+			msjInicial:function(){
+				servicio.objeto['mensaje']='Saludos desde la fábrica !';
+			},
+			msjNuevo:function(msj){
+				servicio.objeto['mensaje']=msj;
+			}
+		};
+		return servicio;
+	})
+	;
