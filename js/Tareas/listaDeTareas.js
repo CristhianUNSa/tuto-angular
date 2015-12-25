ad0454eb392bc1f633736308f3beeee0b1c4e2ce
Cristhian <valenciacristhian47@gmail.com>
2015-12-25 10:43:02 -0300

    Video 3 de la serie de videotutoriales de Israel Guzman

--- js/Tareas/listaDeTareas.js
+++ js/Tareas/listaDeTareas.js
@@ -4,4 +4,24 @@ angular.module('todoAp',[])
 		{texto:'Super Heroico con AngularJS', hecho:true},
 		{texto:'Crear una APP con Angular',hecho:false}
 	];
+	$scope.agregarTarea=function(){
+		$scope.tareas.push(
+			{texto:$scope.textoNuevaTarea,hecho:false}
+		);
+		$scope.textoNuevaTarea='';
+	};
+	$scope.restantes=function(){
+		var cuenta=0;
+		angular.forEach($scope.tareas,function(tarea){
+			cuenta+=tarea.hecho ? 0 : 1;
+		});
+		return cuenta;
+	};
+	$scope.eliminar=function(){
+		var tareasViejas=$scope.tareas;
+		$scope.tareas=[];
+		angular.forEach(tareasViejas,function(tarea){
+			if(!tarea.hecho) $scope.tareas.push(tarea);
+		});
+	};
 }]);
